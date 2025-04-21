49591311a20da1558d17649ae66d720f839d75e0
xueover666 <61985801+xueover666@users.noreply.github.com>
2025-04-21 15:14:30 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -213,7 +213,7 @@ async function sendNotify(
   text,
   desp,
   params = {},
-  author = '\n\n本通知 By：https://github.com/whyour/qinglong',
+  author = '\n\n本通知 By：夜晚影视剧',
 ) {
   //提供6种通知
   desp += author; //增加作者信息，防止被贩卖等
